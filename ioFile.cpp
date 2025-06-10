a8f723b509d9ebb232adbe70994f1c17e62f1e68
Faizaditya-oss <faiz.aditya.ft24@mail.umy.ac.id>
2025-06-10 15:01:50 +0700

    unlimited loop

--- ioFile.cpp
+++ ioFile.cpp
@@ -12,4 +12,8 @@ int main () {
     outfile.open("contohfile.txt");
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    // unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+    
 }
