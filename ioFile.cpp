6cdcd9faec3c6308438c049df4a8095a635d3456
Faizaditya-oss <faiz.aditya.ft24@mail.umy.ac.id>
2025-06-10 15:03:00 +0700

    selesai dalam menulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -20,3 +20,7 @@ int main () {
         //loop akan berhenti jika memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
+    
+    // selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+   
