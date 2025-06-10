1d1752c567ce97d5712b43ca0bf30d701b8a6ef4
Faizaditya-oss <faiz.aditya.ft24@mail.umy.ac.id>
2025-06-10 15:24:37 +0700

    blok isi akan di eksekusi

--- manualException.cpp
+++ manualException.cpp
@@ -8,4 +8,11 @@ int main()
         throw 0.5; // melemparkan sebuah intetrger maka
         cout << "Pernyataan tidak akan di eksekusi" <<endl;
     }
+    catch (int a) {
+        // blok isi akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" <<endl;
+    }
+    catch (...) {
+       
+    }
 }
