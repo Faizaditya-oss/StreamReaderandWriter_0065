0c37834dbcaaf5743f77b48d10b69d8955d3fd78
Faizaditya-oss <faiz.aditya.ft24@mail.umy.ac.id>
2025-06-10 15:02:38 +0700

    saat loop berhenti

--- ioFile.cpp
+++ ioFile.cpp
@@ -15,5 +15,8 @@ int main () {
     // unlimited loop untuk menulis
     while (true) {
         cout << "- ";
-    
-}
+        //mendapatkan setiap karakter dalam satu baris
+        getline (cin, baris);
+        //loop akan berhenti jika memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
