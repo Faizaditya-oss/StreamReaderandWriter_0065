f6a6f6cc434b1ede1cbc3cd4caff794b7952a5af
Faizaditya-oss <faiz.aditya.ft24@mail.umy.ac.id>
2025-06-10 15:32:10 +0700

    unlimited loop untuk menulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -14,4 +14,9 @@ int main () {
     ofstream outfile;
     // menunjuk ke sebuah nama file
     outfile.open(NamaFile + ".txt", ios::out);
+    cout << ">= menulis file, \'q\' untuk keluar" <<endl;
+    // unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+    }
 }
