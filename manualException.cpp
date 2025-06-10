d53721b753103f30275c156639a17145015aac9f
Faizaditya-oss <faiz.aditya.ft24@mail.umy.ac.id>
2025-06-10 15:18:31 +0700

    untuk obyek exception

---
+++ manualException.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" <<endl;
+        throw 0.5; // melemparkan sebuah intetrger maka
+        cout << "Pernyataan tidak akan di eksekusi" <<endl;
+    }
+    catch (int a) {
+        // blok isi akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" <<endl;
+    }
+    catch (...) {
+        
+    }
+}
